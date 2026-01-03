c256abcc3c9b0306ff48e87dd31c960b8b6dd0b4
Dragan Gyorgiev <dragan.gyorgiev@snp4you.com>
2026-01-03 15:50:23 +0100

    Rerouting fix

--- src/pages/error/page-not-found.ts
+++ src/pages/error/page-not-found.ts
@@ -17,7 +17,7 @@ export class PageNotFound extends LitElement {
 			      <p class="mt-6 text-lg font-medium text-pretty text-white/70 sm:text-xl/8">Sorry, we couldn’t find the page
 				      you’re looking for.</p>
 			      <div class="mt-10 flex justify-center">
-				      <a href="#" class="text-sm/7 font-semibold text-white"><span aria-hidden="true">&larr;</span> Back to home</a>
+				      <a href="/" class="text-sm/7 font-semibold text-white"><span aria-hidden="true">&larr;</span> Back to home</a>
 			      </div>
 		      </div>
 	      </main>
