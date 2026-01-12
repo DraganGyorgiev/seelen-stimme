7927e4192bd5670aa75fbb8a4ef828a3d500bb17
Dragan Gyorgiev <dragan.gyorgiev@snp4you.com>
2026-01-12 16:51:47 +0100

    Testemonials text refactor.

--- src/components/testemonials/customer-review.ts
+++ src/components/testemonials/customer-review.ts
@@ -11,7 +11,10 @@ export class CustomerReview extends LitElement {
 					<div class="ml-[-22rem] aspect-1313/771 w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-linear-to-tr from-[#008080] to-[#4bc0f2] xl:mr-[calc(50%-12rem)] xl:ml-0" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
 				</div>
 				<div class="absolute inset-y-0 bottom-40 -z-10 translate-y-1/4 transform-gpu overflow-hidden opacity-30 blur-3xl" aria-hidden="true">
-					<div class="mr-[max(50%,38rem)] aspect-1313/771 w-[82.0625rem] bg-linear-to-tr from-[#008080] to-[#4bc0f2]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+					<div 
+						class="mr-[max(50%,38rem)] aspect-1313/771 w-[82.0625rem] bg-linear-to-tr from-[#008080] to-[#4bc0f2]" 
+						style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+					</div>
 				</div>
 				<div class="mx-auto max-w-7xl px-6 lg:px-8">
 					<div class="mx-auto max-w-2xl text-center">
@@ -20,11 +23,12 @@ export class CustomerReview extends LitElement {
 					<div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm/6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-rows-1 xl:grid-flow-col xl:grid-cols-4">
 						<figure class="rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
 							<blockquote class="p-6 text-lg font-semibold tracking-tight text-gray-900 sm:p-12 sm:text-xl/8">
-								<p>“1000 Dank nochmal... Ich habe sowas noch nie erlebt... Es hat alles gestimmt, sooo viele Informationen, das hättest du niemals von irgendjemand wissen können... Alles hat zu 100 Prozent gepasst... Es war magisch!!! Ich danke dir von ganzem Herzen!!!!”</p>
+								<p>“1000 Dank nochmal... Ich habe sowas noch nie erlebt... Es hat alles gestimmt, sooo viele Informationen, das hättest du niemals von irgendjemand wissen können... 
+									Alles hat zu 100 Prozent gepasst... Es war magisch!!! Ich danke dir von ganzem Herzen!!!!”</p>
 							</blockquote>
 							<figcaption class="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
 								<div class="flex-auto">
-									<div class="font-semibold">Brenna Goyette</div>
+									<div class="font-semibold">Iris Hoffner</div>
 								</div>
 							</figcaption>
 						</figure>
@@ -36,7 +40,7 @@ export class CustomerReview extends LitElement {
 									</blockquote>
 									<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
 										<div>
-											<div class="font-semibold">Stojanka Alexandropulus</div>
+											<div class="font-semibold">Papadopoulos</div>
 										</div>
 									</figcaption>
 								</figure>
@@ -49,7 +53,7 @@ export class CustomerReview extends LitElement {
 									</blockquote>
 									<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
 										<div>
-											<div class="font-semibold">Bla bla</div>
+											<div class="font-semibold">Birgit Steiner</div>
 										</div>
 									</figcaption>
 								</figure>
@@ -59,8 +63,7 @@ export class CustomerReview extends LitElement {
 							<div class="space-y-8 xl:row-start-1">
 								<figure class="rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5">
 									<blockquote class="text-gray-900 p-6">
-										<p>“
-											Sie nahm Kontakt zu meinem Bruder auf und es war der Wahnsinn, sie hat seinen Charakter genau beschrieben, sie nannte mir Einzelheiten zur Todesursache und sie hat mir Botschaften übermittelt, die genau zu meiner aktuellen Lebenslage passen.
+										<p>“Sie nahm Kontakt zu meinem Bruder auf und es war der Wahnsinn, sie hat seinen Charakter genau beschrieben, sie nannte mir Einzelheiten zur Todesursache und sie hat mir Botschaften übermittelt, die genau zu meiner aktuellen Lebenslage passen.
 											Liebe Stefi, ich bin die unheimlich dankbar für diesen tollen und sehr emotionalen Kontakt. Vielen, vielen Dank!”</p>
 									</blockquote>
 									<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
@@ -84,7 +87,7 @@ export class CustomerReview extends LitElement {
 									</blockquote>
 									<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
 										<div>
-											<div class="font-semibold">Tom Cook</div>
+											<div class="font-semibold">Traude</div>
 										</div>
 									</figcaption>
 								</figure>
@@ -97,7 +100,7 @@ export class CustomerReview extends LitElement {
 										<p>“Wir möchten uns ganz herzlich bei der lieben Stefi bedanken für den wundervollen Kontakt unseres Sohns Julian
 											Alles war so stimmig und auch neue Erkenntnisse gebracht. Sie arbeitet sehr beweisführend. Das dürfte ich heute selbst erleben
 											Ich danke dir von und kann die sehr gerne weiter empfehlen”</p>
-									</blockquot px-6e>y<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
+									</blockquote><figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
 										<div>
 											<div class="font-semibold">Leonard Krasner</div>
 										</div>
@@ -112,7 +115,7 @@ export class CustomerReview extends LitElement {
 									</blockquote>
 									<figcaption class="mt-6 flex-wrap flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
 										<div>
-											<div class="font-semibold">Leonard Krasner</div>
+											<div class="font-semibold">Manuela Eder</div>
 										</div>
 									</figcaption>
 								</figure>
